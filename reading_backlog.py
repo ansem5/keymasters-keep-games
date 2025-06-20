c01026150f2f45bdd1beb43935ba42baf9f7ca8c
ansem5 <82561924+ansem5@users.noreply.github.com>
2025-06-20 16:33:04 -0700

    Add files via upload
    not finished

---
+++ reading_backlog.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+from typing import List
+
+from dataclasses import dataclass
+
+from Options import OptionSet
+
+from ..game import Game
+from ..game_objective_template import GameObjectiveTemplate
+
+from ..enums import KeymastersKeepGamePlatforms
+
+
+@dataclass
+class ReadingBacklogArchipelagoOptions:
+    game_backlog_game_selection: ReadingBacklogBookSelection
+    game_backlog_actions: ReadingBacklogActions
+
+
+class ReadingBacklogGame(Game):
+    name = "Reading Backlog"
+    platform = KeymastersKeepGamePlatforms.META
+
+    platforms_other = None
+
+    is_adult_only_or_unrated = False
+
+    options_cls = ReadingBacklogArchipelagoOptions
+
+    def optional_game_constraint_templates(self) -> List[GameObjectiveTemplate]:
+        return list()
+
+    def reading_objective_templates(self) -> List[ReadingObjectiveTemplate]:
+        return [
+            ReadingObjectiveTemplate(
+                label="ACTION BOOK",
+                data={"ACTION": (self.actions, 1), "BOOK": (self.books, 1)},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=1,
+            ),
+        ]
+
+    def actions(self) -> List[str]:
+        return sorted(self.archipelago_options.reading_backlog_actions.value)
+
+    def games(self) -> List[str]:
+        return sorted(self.archipelago_options.reading_backlog_game_selection.value)
+
+
+# Archipelago Options
+class ReadingBacklogBookSelection(OptionSet):
+    """
+    Defines which books are in the player's backlog.
+
+    Replace the placeholders with values of your own choosing.
+    """
+
+    display_name = "Reading Backlog Book Selection"
+
+    default = ["Book 1", "Book 2", "..."]
+
+
+class ReadingBacklogActions(OptionSet):
+    """
+    Defines the possible actions that could be required on a book in the backlog.
+
+    You can customize this list to your liking.
+    """
+
+    display_name = "Reading Backlog Actions"
+
+    default = [
+        "READ",
+        "FINISH",
+    ]
